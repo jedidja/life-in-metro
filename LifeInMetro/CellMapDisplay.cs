bab23994f1f06c85be530c724731172c782413aa
Jedidja Bourgeois <jedidja@freshlycoded.ca>
2012-09-10 10:38:24 -0400

    Some XAML optimization
    Code to match
    http://jedidja.typepad.com/blog/2012/09/some-xaml-optimization.html

--- LifeInMetro/CellMapDisplay.cs
+++ LifeInMetro/CellMapDisplay.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI;
+﻿using System.Collections.Generic;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
@@ -11,11 +12,21 @@ namespace LifeInMetro
         private readonly int numberCellsAcross;
         private readonly int numberCellsDown;
 
+        private readonly Dictionary<int, Rectangle> cellIndexDisplayMap;
+
+        private readonly SolidColorBrush blackBrush;
+        private readonly SolidColorBrush whiteBrush;
+
         public CellMapDisplay(Canvas canvas, int numberCellsAcross, int numberCellsDown, int cellSize)
         {
             this.canvas = canvas;
             this.numberCellsAcross = numberCellsAcross;
             this.numberCellsDown = numberCellsDown;
+
+            cellIndexDisplayMap = new Dictionary<int, Rectangle>();
+            
+            blackBrush = new SolidColorBrush(Colors.Black);
+            whiteBrush = new SolidColorBrush(Colors.White);
 
             for (int y = 0; y < numberCellsDown; y++)
             {
@@ -30,15 +41,17 @@ namespace LifeInMetro
                     Canvas.SetLeft(cell, x * cellSize);
                     Canvas.SetTop(cell, y * cellSize);
                     canvas.Children.Add(cell);
+
+                    cellIndexDisplayMap[y * numberCellsAcross + x] = cell;
                 }
             }
         }
 
         public void DrawCell(int x, int y, bool on)
         {
-            var cellRectangle = canvas.Children[y * numberCellsAcross + x] as Rectangle;
+            var cellRectangle = cellIndexDisplayMap[y * numberCellsAcross + x];
 
-            cellRectangle.Fill = on ? new SolidColorBrush(Colors.White) : new SolidColorBrush(Colors.Black);
+            cellRectangle.Fill = on ? whiteBrush : blackBrush;
         }
     }
 }
